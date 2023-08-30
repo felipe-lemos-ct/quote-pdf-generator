c3a65e42e97b50dafce06ecf5e89aa5f67c493c4
Felipe Lemos <>
2023-08-30 19:09:45 +0200

    fix: test after fail

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = require("./routes");
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(8080, () => console.log("server up and running on port 8080"));
+app.listen(PORT, () => console.log(`server up and running on port ${PORT}`));
